aa06e856acde8cbed4affffe983a87ae3bfed9ec
中嶋ひかる <nakayama_bird@nakajimahikarunoMacBook-Air.local>
2024-07-21 14:46:06 +0900

    add: #19 新規投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,10 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   private
     def post_params
       params.require(:post).permit(:genre, :restaurant_name, :address, :body, :amount)
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 255} #name要素を入力必須、255文字まで。
 
   has_many :posts
+
+  def own?(object)
+    object.user_id == id
+  end
 end
