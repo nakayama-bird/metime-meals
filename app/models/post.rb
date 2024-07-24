23fc7b68b40cdcba811c0b13016615b406348713
中嶋ひかる <nakayama_bird@nakajimahikarunoMacBook-Air.local>
2024-07-24 18:09:27 +0900

    add: #60 タグづけ機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,4 +18,19 @@ class Post < ApplicationRecord
   # geocodingについての設定
   geocoded_by :address
   before_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
+
+  def save_with_tags(tag_names:)
+    ActiveRecord::Base.transaction do
+      self.tags = tag_names.map { |name| Tag.find_or_initialize_by(name: name.strip) }
+      save!
+    end
+    true
+  rescue StandardError
+    false
+  end
+
+  def tag_names
+    # NOTE: pluckだと新規作成失敗時に値が残らない(返り値がnilになる)
+    tags.map(&:name).join(',')
+  end
 end
