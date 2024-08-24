620c5afc7d827be792b4d3bb09cec7b413e2a939
中嶋ひかる <nakayama_bird@nakajimahikarunoMacBook-Air.local>
2024-08-24 11:48:12 +0900

    add: #29 地図上での検索機能の実装

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,21 @@
 class MapsController < ApplicationController
-  skip_before_action :require_login, only: %i[index]
+  skip_before_action :require_login, only: %i[index search]
+  before_action :set_search_posts_form
   def index
     @posts = Post.includes(:user).order(created_at: :desc)
+  end
+
+  def search
+    @posts = @search_form.search.includes(:user).order(created_at: :desc)
+  end
+
+  private
+
+  def set_search_posts_form
+    @search_form = SearchPostsForm.new(search_post_params)
+  end
+
+  def search_post_params
+    params.fetch(:q, {}).permit(:address_or_name, :genre_select)
   end
 end
