5bf6bc12873578b3e75b36dd32aa2386713239ce
中嶋ひかる <nakayama_bird@nakajimahikarunoMacBook-Air.local>
2024-08-26 22:50:48 +0900

    add: #99 地図、マイページに関してincludeを使用して改善済

--- app/controllers/mypage/posts_controller.rb
+++ app/controllers/mypage/posts_controller.rb
@@ -1,5 +1,5 @@
 class Mypage::PostsController < ApplicationController
   def index
-    @posts = Post.includes(:user).where(user_id: current_user.id).order(created_at: :desc)
+    @posts = Post.includes(:user, :tags).where(user_id: current_user.id).order(created_at: :desc)
   end
 end
