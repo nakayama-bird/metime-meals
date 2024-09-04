b3183ecd5c2266ac4e5ca645363358c9bee2d5ce
中嶋ひかる <nakayama_bird@nakajimahikarunoMacBook-Air.local>
2024-09-04 21:32:01 +0900

    add: #133 ログイン後、オートログインの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      auto_login(@user)
       redirect_to root_path, success: 'ユーザー登録に成功しました'
     else
       flash.now[:alert] = 'ユーザー登録に失敗しました'
