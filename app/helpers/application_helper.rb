b09245b842c3ef7c662654b0c6c528841268b4b7
中嶋ひかる <nakayama_bird@nakajimahikarunoMacBook-Air.local>
2024-09-10 11:33:49 +0900

    add: #142 各ページのタイトルを動的に出力する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  def page_title(title = '')
+    base_ttitle = 'Me Time Meals'
+    if title.empty?
+      base_ttitle
+    else
+      "#{title} | #{base_ttitle}"
+    end
+  end
+
   def flash_background_color(type)
     case type.to_sym # シンボルに変換
     when :success then 'bg-success'
