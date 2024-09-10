9d8bbcbcc54812a1c0ba3371ddc81f72eba630a8
中嶋ひかる <nakayama_bird@nakajimahikarunoMacBook-Air.local>
2024-09-10 16:07:35 +0900

    fix: #30 ページタイトルがmeta-tagと重複するため一旦削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,4 @@
 module ApplicationHelper
-  def page_title(title = '')
-    base_ttitle = 'Me Time Meals'
-    if title.empty?
-      base_ttitle
-    else
-      "#{title} | #{base_ttitle}"
-    end
-  end
 
   def flash_background_color(type)
     case type.to_sym # シンボルに変換
