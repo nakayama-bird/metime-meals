67b5265658ec6cd2f25919c2aa188b378cc20129
中嶋ひかる <nakayama_bird@nakajimahikarunoMacBook-Air.local>
2024-09-15 13:50:28 +0900

    fix: 画像ファイルを一部pngからwebpに変更

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -24,7 +24,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
     #   # For Rails 3.1+ asset pipeline compatibility:
     #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
     #
-    'default.png'
+    'default.webp'
   end
 
   # Process files as they are uploaded:
