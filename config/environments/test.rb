42915a4f3a8ce3a74f77ad7a6031c24430e66f73
中嶋ひかる <nakayama_bird@nakajimahikarunoMacBook-Air.local>
2024-10-05 13:28:37 +0900

    fix: #180 GitHubActionsの設定だけ残す

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -62,6 +62,4 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.assets.compile = true
-  config.assets.digest = true
 end
