c578e2b347979abb057a302826b04459d12ba803
中嶋ひかる <nakayama_bird@nakajimahikarunoMacBook-Air.local>
2024-10-05 18:41:23 +0900

    fix: #180 Chrome系の設定を再度追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -45,11 +45,15 @@ RSpec.configure do |config|
 
   # システムスペックで事前にCapybaraを設定
   config.before(:each, type: :system) do
+    if ENV['SELENIUM_DRIVER_URL']
       driven_by :remote_chrome
       Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
       Capybara.server_port = 4444
       Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
       Capybara.ignore_hidden_elements = false
+    else
+      driven_by :selenium_chrome_headless
+    end
   end
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
