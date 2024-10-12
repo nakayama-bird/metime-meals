b81fa29630d08d8cc776dbc57ffd861212df6868
中嶋ひかる <nakayama_bird@nakajimahikarunoMacBook-Air.local>
2024-10-12 11:16:01 +0900

    add: #180 システムスペックの動作の安定化のための設定

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,6 +3,9 @@ Capybara.register_driver :remote_chrome do |app|
   options.add_argument('no-sandbox')
   options.add_argument('headless')
   options.add_argument('disable-gpu')
-  options.add_argument('window-size=1680,1050')
+  options.add_argument('window-size=1024,768')
+  options.add_argument('disable-dev-shm-usage') # /dev/shmの使用を無効にしてメモリを直接使用
+  options.add_argument('disable-software-rasterizer') # ソフトウェアラスタライザーを無効にする
+  options.add_argument('disable-extensions') # 拡張機能を無効にする
   Capybara::Selenium::Driver.new(app, browser: :remote, url: ENV['SELENIUM_DRIVER_URL'], capabilities: options)
 end
