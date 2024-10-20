aa0829197e40bb03a40a69a69c09a6810b0ba977
中嶋ひかる <nakayama_bird@nakajimahikarunoMacBook-Air.local>
2024-10-20 20:27:57 +0900

    add: #183 UserSessionスペックの追加

---
+++ spec/system/user_sessions_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'UserSessions', type: :system do
+  let(:user) { create(:user) }
+
+  describe 'ログイン前' do
+    context 'フォームの入力値が正常' do
+      it 'ログイン処理が成功する' do
+        visit login_path
+        fill_in 'メールアドレス', with: user.email
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログイン'
+        expect(page).to have_content 'ログインに成功しました'
+        expect(current_path).to eq maps_path
+      end
+    end
+
+    context 'フォームが未入力' do
+      it 'ログイン処理が失敗する' do
+        visit login_path
+        fill_in 'メールアドレス', with: ''
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログイン'
+        expect(page).to have_content 'ログインに失敗しました'
+        expect(current_path).to eq login_path
+      end
+    end
+  end
+
+  describe 'ログイン後' do
+    context 'ログアウトボタンをクリック' do
+      it 'ログアウト処理が成功する' do
+        login_as(user)
+        click_link 'ログアウト', match: :first
+        # ログアウトが成功したことを確認するテスト
+        page.accept_alert 'ログアウトしてもよろしいですか？'
+        expect(page).to have_content 'ログイン'
+        visit current_path
+        expect(current_path).to eq root_path
+      end
+    end
+  end
+end
