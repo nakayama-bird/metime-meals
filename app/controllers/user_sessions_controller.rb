90162eb85d1312f8498a15a0989ebddc5527164e
中嶋ひかる <nakayama_bird@nakajimahikarunoMacBook-Air.local>
2024-10-20 19:51:26 +0900

    fix: #183 ログイン後の画面遷移を地図一覧に変更

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,7 +5,7 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_to root_path, success: 'ログインに成功しました'
+      redirect_to maps_path, success: 'ログインに成功しました'
     else
       flash.now[:alert] = 'ログインに失敗しました'
       render :new, status: :unprocessable_entity
